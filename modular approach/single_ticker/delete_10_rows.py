2f59c6da1df8465522ec9d939dab213db2d83d99
matmal1992 <mateusz.malek.programista@gmail.com>
2026-02-23 11:08:04 +0100

    Keep date indexes in delete_10_rows script

---
+++ modular approach/single_ticker/delete_10_rows.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import os
+
+print("Aktualny katalog roboczy:")
+print(os.getcwd())
+
+print("Pliki w folderze:")
+for f in os.listdir():
+    print(f)
+
+df = pd.read_parquet("CRI_WA.parquet")
+df = df.iloc[:-10]
+df.to_parquet("CRI_WA.parquet")
