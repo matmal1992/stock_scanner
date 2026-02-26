61390d3433f2a7f2b4c8ecfee8b12a430b369496
matmal1992 <mateusz.malek.programista@gmail.com>
2026-02-26 15:30:32 +0100

    Update main.py and some filenames

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from download import d1_down_gpw
-from download import d1_update_gpw
-from analysis import print_tickers_for_15m
+from download.d1 import d1_down_gpw
+from download.d1 import d1_update_gpw
+from analysis import first_tier_filter
+from download.min15 import min15_down
+from download.min15 import min15_update
+from analysis import second_tier_filter
 
 
 def main():
@@ -11,7 +14,16 @@ def main():
     d1_update_gpw.main()
 
     print("\n===== ETAP 3: ANALIZA =====")
-    print_tickers_for_15m.main()
+    first_tier_filter.main()
+
+    print("\n===== ETAP 4: DOWNLOAD 15min tickers =====")
+    min15_down.main()
+
+    print("\n===== ETAP 5: UPDATE =====")
+    min15_update.main()
+
+    print("\n===== ETAP 6: ANALIZA =====")
+    second_tier_filter.main()
 
     print("\n===== GOTOWE =====")
 
