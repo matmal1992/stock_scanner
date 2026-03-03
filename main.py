c1bae3e14235e4370fcd6d5caa2fb3a125ca698a
matmal1992 <129147442+matmal1992@users.noreply.github.com>
2026-03-03 11:45:08 +0100

    Second tier filter (#2)
    * Add files for second tier filter
    
    * Fix second tier filter downloading script
    
    * Add update second tier filter ticket script
    
    * Add second tier filter
    
    * Update main.py and some filenames
    
    * Update paths in downloading first tier tickers
    
    * Add confirming window and print list of tickers for next filter
    
    * Add log_file for 1st tier filter
    
    * Add tasks.json to vscode
    
    * Skip tickers with actual data
    
    * Update results in raport
    
    * Add gitignore and modify d1_down_gpw
    
    * Update first tier download data
    
    * Update report printing
    
    * update gitignore
    
    * Tidy up a little bit
    
    * Split downloading script into functions
    
    * Add last update.txt and fix downloading sctipt

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
-from download import d1_down_gpw
-from download import d1_update_gpw
-from analysis import print_tickers_for_15m
+from download.d1 import d1_down_gpw
+from download.d1 import d1_update_gpw
+from analysis import first_tier_filter
+from download.min15 import min15_down
+from download.min15 import min15_update
+from analysis import second_tier_filter
+import tkinter as tk
+from tkinter import messagebox
 
-
+def confirm_continue(messege):
+    root = tk.Tk()
+    root.withdraw()  # ukrywa główne okno
+    answer = messagebox.askyesno("Potwierdzenie", messege)
+    root.destroy()
+    return answer
+    
 def main():
+    # confirm_continue("Download first tier data")
     print("===== ETAP 1: DOWNLOAD =====")
     d1_down_gpw.main()
 
-    print("\n===== ETAP 2: UPDATE =====")
-    d1_update_gpw.main()
+    # # confirm_continue("Analyze first tier data")
+    # print("\n===== ETAP 3: ANALIZA =====")
+    # # first_tier_filter.main()
 
-    print("\n===== ETAP 3: ANALIZA =====")
-    print_tickers_for_15m.main()
+    # # confirm_continue("Download second tier data")
+    # print("\n===== ETAP 4: DOWNLOAD 15min tickers =====")
+    # # min15_down.main()
+
+    # # confirm_continue("Analyze second tier data")
+    # print("\n===== ETAP 6: ANALIZA =====")
+    # # second_tier_filter.main()
 
     print("\n===== GOTOWE =====")
-
 
 if __name__ == "__main__":
     main()
--- sand_box/single_ticker/delete_10_rows.py
+++ sand_box/single_ticker/delete_10_rows.py
@@ -8,6 +8,6 @@ print("Pliki w folderze:")
 for f in os.listdir():
     print(f)
 
-df = pd.read_parquet("CRI_WA.parquet")
-df = df.iloc[:-10]
-df.to_parquet("CRI_WA.parquet")
+df = pd.read_parquet("11B_WA.parquet")
+df = df.iloc[:-1]
+df.to_parquet("11B_WA.parquet")
