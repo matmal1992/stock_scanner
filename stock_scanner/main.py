98050683823594d01654fc68033a386de2a10540
matmal1992 <mateusz.malek.programista@gmail.com>
2026-03-28 10:26:14 +0100

    Add .flake8 to the repo

--- stock_scanner/main.py
+++ stock_scanner/main.py
@@ -1,4 +1,4 @@
-from stock_scanner.config import CONFIG_1D, CONFIG_5M, CONFIG_15M
+from stock_scanner.stock_scanner.config import CONFIG_1D, CONFIG_5M, CONFIG_15M
 from download.downloader import run_download
 from stock_scanner.profiles import PROFILE_T1, PROFILE_T2, PROFILE_T3
 from report.report_builder import create_empty_report
@@ -9,19 +9,19 @@ from stock_scanner.strategy_profiles import *
 def main():
     report_path = create_empty_report()
     print("===== STAGE 1: DOWNLOAD first tier tickers =====")
-    run_download(CONFIG_1D, "<!-- T1_DOWNLOAD -->", "first")
+    # run_download(CONFIG_1D, "<!-- T1_DOWNLOAD -->", "first")
 
     print("\n===== ETAP 2: ANALIZA first tier tickers =====")
     run_scan(PROFILE_T1)
 
     print("\n===== STAGE 3: DOWNLOAD second tier tickers =====")
-    run_download(CONFIG_15M, "<!-- T2_DOWNLOAD -->", "second")
+    # run_download(CONFIG_15M, "<!-- T2_DOWNLOAD -->", "second")
 
     print("\n===== ETAP 4: ANALIZA 2nd tier tickers =====")
     run_scan(PROFILE_T2)
 
     print("\n===== STAGE 5: DOWNLOAD third tier tickers =====")
-    run_download(CONFIG_5M, "<!-- T3_DOWNLOAD -->", "third")
+    # run_download(CONFIG_5M, "<!-- T3_DOWNLOAD -->", "third")
 
     print("\n===== ETAP 6: ANALIZA third tier tickers =====")
     run_scan(PROFILE_T3)
