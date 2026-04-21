1574f711cf61bdb521d00aec514898912ef79eba
matmal1992 <mateusz.malek.programista@gmail.com>
2026-04-21 23:10:34 +0200

    Add separate function to run 3 tier strategy

--- stock_scanner/main.py
+++ stock_scanner/main.py
@@ -1,12 +1,13 @@
-# from stock_scanner.config import CONFIG_1D, CONFIG_5M, CONFIG_15M
-# from stock_scanner.download.downloader import run_download
-# from stock_scanner.profiles import PROFILE_T1, PROFILE_T2, PROFILE_T3
-# from stock_scanner.scanners.universal_scanner import run_scan
 import sys
 from pathlib import Path
 
 from PySide6.QtQml import QQmlApplicationEngine
 from PySide6.QtWidgets import QApplication
+
+from stock_scanner.config import CONFIG_1D, CONFIG_5M, CONFIG_15M
+from stock_scanner.download.downloader import run_download
+from stock_scanner.profiles import PROFILE_T1, PROFILE_T2, PROFILE_T3
+from stock_scanner.scanners.universal_scanner import run_scan
 
 
 def main() -> None:
@@ -24,25 +25,28 @@ def main() -> None:
 
 
 # def main() -> None:
-#     print("===== STAGE 1: DOWNLOAD first tier tickers =====")
-#     run_download(CONFIG_1D, "<!-- T1_DOWNLOAD -->", "first")
 
-#     print("\n===== ETAP 2: ANALIZA first tier tickers =====")
-#     run_scan(PROFILE_T1)
 
-#     print("\n===== STAGE 3: DOWNLOAD second tier tickers =====")
-#     run_download(CONFIG_15M, "<!-- T2_DOWNLOAD -->", "second")
+def run_3t_strategy() -> None:
+    print("===== STAGE 1: DOWNLOAD first tier tickers =====")
+    run_download(CONFIG_1D, "<!-- T1_DOWNLOAD -->", "first")
 
-#     print("\n===== ETAP 4: ANALIZA 2nd tier tickers =====")
-#     run_scan(PROFILE_T2)
+    print("\n===== ETAP 2: ANALIZA first tier tickers =====")
+    run_scan(PROFILE_T1)
 
-#     print("\n===== STAGE 5: DOWNLOAD third tier tickers =====")
-#     run_download(CONFIG_5M, "<!-- T3_DOWNLOAD -->", "third")
+    print("\n===== STAGE 3: DOWNLOAD second tier tickers =====")
+    run_download(CONFIG_15M, "<!-- T2_DOWNLOAD -->", "second")
 
-#     print("\n===== ETAP 6: ANALIZA third tier tickers =====")
-#     run_scan(PROFILE_T3)
+    print("\n===== ETAP 4: ANALIZA 2nd tier tickers =====")
+    run_scan(PROFILE_T2)
 
-#     print("\n===== DONE =====")
+    print("\n===== STAGE 5: DOWNLOAD third tier tickers =====")
+    run_download(CONFIG_5M, "<!-- T3_DOWNLOAD -->", "third")
+
+    print("\n===== ETAP 6: ANALIZA third tier tickers =====")
+    run_scan(PROFILE_T3)
+
+    print("\n===== DONE =====")
 
 
 if __name__ == "__main__":
