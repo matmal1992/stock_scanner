74a2260d1158cff306eb129d22e80738f5b43b7a
matmal1992 <mateusz.malek.programista@gmail.com>
2026-05-22 15:24:54 +0200

    Get max rss feed

--- stock_scanner/ui/workers/rss_feed_worker.py
+++ stock_scanner/ui/workers/rss_feed_worker.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import feedparser
 from PySide6.QtCore import QObject, Signal
 
@@ -9,16 +11,27 @@ class RSSWorker(QObject):
     data_ready = Signal(list)
 
     def run(self) -> None:
-        # url = "https://biznes.pap.pl/rss/firmy"
-        url = "https://www.bankier.pl/rss/wiadomosci.xml"
+        url = "https://biznes.pap.pl/rss"
+        # url = "https://www.bankier.pl/rss/wiadomosci.xml"
         self.log.emit("Start pobierania RSS from " + url)
+        today = datetime.now().date()
 
         try:
-            feed = feedparser.parse(url)
+            feed = feedparser.parse(url, request_headers={"User-Agent": "Mozilla/5.0"})
+            today_entries = []
 
-            self.log.emit(f"Entries: {len(feed.entries)}")
+            for entry in feed.entries:
+                if not hasattr(entry, "published_parsed"):
+                    continue
 
-            titles = [e.title for e in feed.entries[:5]]
+                entry_date = datetime(*entry.published_parsed[:6]).date()
+
+                if entry_date == today:
+                    today_entries.append(entry)
+
+            self.log.emit(f"Entries: {len(today_entries)}")
+
+            titles = [e.title for e in today_entries]
             self.data_ready.emit(titles)
 
         except Exception as e:
