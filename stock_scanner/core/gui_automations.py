faee2855a0e16630d601b974b0b672789dbb1c58
matmal1992 <mateusz.malek.programista@gmail.com>
2026-07-07 21:26:21 +0200

    Detecting last copy icon works well

--- stock_scanner/core/gui_automations.py
+++ stock_scanner/core/gui_automations.py
@@ -43,7 +43,7 @@ def click_icon_and_scroll(template_path: str, threshold: float = 0.85) -> Option
     original_point = (x, y)
     pyautogui.click(x, y)
     print(f"Kliknięto ikonę: ({x}, {y}) score={score}")
-    time.sleep(3)
+    time.sleep(1)
     pyautogui.click(x - 200, y)
     print("Kliknięto ikonę: w wolne pole")
 
@@ -54,10 +54,48 @@ def click_icon_and_scroll(template_path: str, threshold: float = 0.85) -> Option
     return original_point
 
 
+def find_and_click_last_copy_icon(template_path: str, threshold: float = 0.85) -> None:
+    with mss.MSS() as sct:
+        monitor = sct.monitors[0]
+        screenshot = sct.grab(monitor)
+
+        img = np.array(screenshot)
+        img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+
+    template = cv2.imread(template_path, cv2.IMREAD_COLOR)
+    h, w = template.shape[:2]
+
+    result = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
+
+    locations = np.where(result >= threshold)
+
+    print(f"Znaleziono {len(locations[0])} dopasowań")
+
+    centers = []
+
+    for pt in zip(*locations[::-1]):
+        x, y = pt
+
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        center_x = x + w // 2
+        center_y = y + h // 2
+        centers.append({"x": center_x, "y": center_y})
+        pyautogui.moveTo(center_x, center_y, duration=1)
+        cv2.circle(img, (center_x, center_y), 5, (0, 0, 255), -1)
+
+    bottom = max(centers, key=lambda p: p["y"])
+    pyautogui.moveTo(bottom["x"], bottom["y"], duration=2)
+    pyautogui.click(bottom["x"], bottom["y"])
+
+    cv2.imshow("DEBUG", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
 if __name__ == "__main__":
-    import time
-
-    print("Start za 3 sekundy...")
-    time.sleep(3)
-
-    click_icon_and_scroll("stock_scanner/assets/zapytaj.png")
+    print("Start soon...")
+    time.sleep(1)
+    click_icon_and_scroll("stock_scanner/assets/ask_anything.png")
+    time.sleep(1)
+    find_and_click_last_copy_icon("stock_scanner/assets/copy_icon.png")
